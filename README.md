aacf83f6900dfb7bae7d715a52e180ee0315ea96
rvollmer <rvollmer@aypac.de>
2016-11-19 03:04:15 +0100

    initial commit, version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Arduino-TR-064-SOAP-Library
-Arduino library for the TR-064 protocol, most commonly used by Fritz!Box
+Arduino library for the TR-064 protocol, most commonly used by Fritz!Box. A [Description of the protocol](https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AVM_TR-064_first_steps.pdf).
+
+Has been developed on an ESP8266 and was tested with a Fritz!Box FON WLAN 7360.
+
+# TODO
+Help is very welcome (especially when improving the library code). I'm not a genius with C++.
+##Library
+*Test for different setups (i.e. other microcontroller and other routers)
+*Make code more efficient
+*Do some (better) error-handeling
+	*Return proper errors
+	*Re-initialize authorization to have a fresh Nonce
+*Add some more commentary
+*Create a sophisticated debug/verbose mode (and silence the non-debug mode!)
+
+##Example
+*Add nice GUI/Website to configure/manage the users/LEDs/MACs/known devices
---
+++ TR-064-SOAP-Library/tr064.h
@@ -0,0 +1,50 @@
+/*
+  tr064.h - Library for communicating via TR-064 protocol
+  (e.g. Fritz!Box)
+  A descriptor of the protocol can be found here: https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AVM_TR-064_first_steps.pdf
+  The latest Version of this library can be found here: http://github.com/Aypac
+  Created by René Vollmer, November 2016
+*/
+
+
+#ifndef tr064_h
+#define tr064_h
+
+#define USE_SERIAL Serial
+#include "Arduino.h"
+#include <ESP8266HTTPClient.h>
+
+#define arr_len( x )  ( sizeof( x ) / sizeof( *x ) )
+
+class TR064
+{
+  public:
+    TR064(int port, String ip, String user, String pass);
+    void init();
+    String action(String service, String act);
+    String action(String service, String act, String params[][2], int nParam);
+    String action(String service, String act, String params[][2], int nParam, String (*req)[2], int nReq);
+    String xmlTakeParam(String inStr,String needParam);
+    String md5String(String s);
+    String byte2hex(byte number);
+  private:
+    void initServiceURLs();
+    void initNonce();
+    String httpRequest(String url, String xml, String action);
+    String generateAuthToken();
+    String generateAuthXML();
+    String findServiceURL(String service);
+    String xmlTakeParami(String inStr,String needParam);
+    String _ip;
+    int _port;
+    String _user;
+    String _pass;
+    String _realm; //To be requested from the router
+    String _secretH; //to be generated
+    String _nonce = "";
+    const String _requestStart = "<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">";
+    const String _detectPage = "/tr64desc.xml";
+    String _services[100][2];
+};
+
+#endif
